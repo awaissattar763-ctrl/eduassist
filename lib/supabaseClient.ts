c59c3ef294d64b9464661982ee704b5b14398605
awaissattar763-ctrl <awaissattar763@gmail.com>
2026-04-09 05:17:42 -0400

    Add global headers to Supabase client initialization

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,4 +4,11 @@ const supabaseUrl = "https://flbbfwlyaunxjnvjqoek.supabase.co";
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsYmJmd2x5YXVueGpudmpxb2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NDYyNTgsImV4cCI6MjA5MTEyMjI1OH0.GzOmQ6Ie2jGxjyRwLf8iPNEhS6Vo3RKWwraRgN5D-eI";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  global: {
+    headers: {
+      apikey: supabaseKey,
+      Authorization: `Bearer ${supabaseKey}`,
+    },
+  },
+});
