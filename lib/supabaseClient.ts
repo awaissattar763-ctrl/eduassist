3ba53364e9f27df4b0cf38eb1607280121192f09
awaissattar763-ctrl <awaissattar763@gmail.com>
2026-04-11 03:13:34 -0400

    Fix formatting in supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://flbbfwlyaunxjnvjqoek.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsYmJmd2x5YXVueGpudmpxb2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NDYyNTgsImV4cCI6MjA5MTEyMjI1OH0.GzOmQ6Ie2j2GxjyRwLf8iPNEhS6Vo3RKWwraRgN5D-eI"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsYmJmd2x5YXVueGpudmpxb2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NDYyNTgsImV4cCI6MjA5MTEyMjI1OH0.GzOmQ6Ie2jGxjyRwLf8iPNEhS6Vo3RKWwraRgN5D-eI"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
